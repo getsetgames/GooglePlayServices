c0a6c1a7ec7453a6285542c392213c5edfec7718
Robert Segal <rob@getsetgames.com>
2016-11-07 10:50:15 -0500

    UE4 UBT file for building this module

---
+++ Source/GooglePlayServices/GooglePlayServices.Build.cs
@@ -0,0 +1,65 @@
+//
+//  Created by Robert Segal on 2016-11-07.
+//  Copyright (c) 2016 Get Set Games Inc. All rights reserved.
+//
+
+using System.IO;
+using System.Diagnostics;
+
+namespace UnrealBuildTool.Rules
+{
+	public class GooglePlayServices : ModuleRules
+	{
+		public GooglePlayServices(TargetInfo Target)
+		{
+			PublicIncludePaths.AddRange(
+				new string[] {
+					// ... add public include paths required here ...
+				}
+				);
+
+			PrivateIncludePaths.AddRange(
+				new string[] {
+					"Developer/GooglePlayServices/Private",
+					// ... add other private include paths required here ...
+				}
+				);
+
+			PublicDependencyModuleNames.AddRange(
+				new string[]
+				{
+					"Core",
+					"CoreUObject",
+					"Engine"
+					// ... add other public dependencies that you statically link with here ...
+				}
+				);
+
+			PrivateDependencyModuleNames.AddRange(
+				new string[]
+				{
+					// ... add private dependencies that you statically link with here ...
+				}
+				);
+
+			DynamicallyLoadedModuleNames.AddRange(
+				new string[]
+				{
+					// ... add any modules that your module loads dynamically here ...
+				}
+				);
+				
+			PrivateIncludePathModuleNames.AddRange(
+			new string[] {
+				"Settings"
+			}
+			);
+
+			if(Target.Platform == UnrealTargetPlatform.Android)
+			{
+				string PluginPath = Utils.MakePathRelativeTo(ModuleDirectory, BuildConfiguration.RelativeEnginePath);
+				AdditionalPropertiesForReceipt.Add(new ReceiptProperty("AndroidPlugin", Path.Combine(PluginPath, "GooglePlayServices_APL.xml")));
+			}
+		}
+	}
+}
